d468e47d69ec8b44a555ffda59cecd702ea23570
George Muljono <muljono.george@gmail.com>
2026-09-25 21:57:47 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+navToggle.addEventListener("click", () => {
+    const isOpen = navLinks.classList.toggle("open");
+    navToggle.setAttribute("aria-expanded", isOpen);
+});
+
+navLinks.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("open");
+        navToggle.setAttribute("aria-expanded", "false");
+    });
+});
